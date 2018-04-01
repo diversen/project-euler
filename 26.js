c9e81a8251017044ba47234436a6a8bf73070526
Dennis Iversen <dennis.iversen@gmail.com>
2018-04-01 15:10:30 +0200

    auto commit

--- 26.js
+++ 26.js
@@ -1,39 +1,35 @@
-/*
-A unit fraction contains 1 in the numerator. The decimal representation of the
-unit fractions with denominators 2 to 10 are given:
-
-1/2 =   0.5 1/3 =   0.(3) 1/4 =   0.25 1/5 =   0.2 1/6 =   0.1(6) 1/7 =
-0.(142857) 1/8 =   0.125 1/9 =   0.(1) 1/10    =   0.1 Where 0.1(6) means
-0.166666..., and has a 1-digit recurring cycle. It can be seen that 1/7 has a
-6-digit recurring cycle.
-
-Find the value of d < 1000 for which 1/d contains the longest recurring cycle in
-its decimal fraction part.
-
----
-
-Something like this: 
-
-A fraction in lowest terms with a prime denominator other than 2 or 5 (i.e.
-coprime to 10) always produces a repeating decimal. The length of the repetend
-(period of the repeating decimal) of 1/p is equal to the order of 10 modulo p.
-If 10 is a primitive root modulo p, the repetend length is equal to p − 1; if
-not, the repetend length is a factor of p − 1. This result can be deduced from
-Fermat's little theorem, which states that 10p−1 ≡ 1 (mod p).
-
-1/n is always a fraction in lowest terms
-
----
-
-Well, this is longer story, so I ended up reading some math. And also created
-some functions to do the math.
-
-A great math explantion I have found on finding numbers of decimals in a unit
-fraction can be seen here: 
-
-https://www.youtube.com/watch?v=lMrz7ISoDGs&index=54&list=PLIljB45xT85AYIeGfDQwHM8i6PQEDnnTI
-
-*/
+/**
+ * A unit fraction contains 1 in the numerator. The decimal representation of
+ * the unit fractions with denominators 2 to 10 are given:
+ *
+ * 1/2 =   0.5 1/3 =   0.(3) 1/4 =   0.25 1/5 =   0.2 1/6 =   0.1(6) 1/7 =
+ * 0.(142857) 1/8 =   0.125 1/9 =   0.(1) 1/10    =   0.1 Where 0.1(6) means
+ * 0.166666..., and has a 1-digit recurring cycle. It can be seen that 1/7 has a
+ * 6-digit recurring cycle.
+ *
+ * Find the value of d < 1000 for which 1/d contains the longest recurring cycle
+ * in its decimal fraction part.
+ *
+ * Something like this: 
+ *
+ * A fraction in lowest terms with a prime denominator other than 2 or 5 (i.e.
+ * coprime to 10) always produces a repeating decimal. The length of the
+ * repetend (period of the repeating decimal) of 1/p is equal to the order of 10
+ * modulo p. If 10 is a primitive root modulo p, the repetend length is equal to
+ * p − 1; if not, the repetend length is a factor of p − 1. This result can be
+ * deduced from Fermat's little theorem, which states that 10p−1 ≡ 1 (mod p).
+ *
+ * 1/n is always a fraction in lowest terms
+ *
+ * Well, this is longer story, so I ended up reading some math. And also created
+ * some functions to do the math.
+ *
+ * A great math explantion I have found on finding numbers of decimals in a unit
+ * fraction can be seen here: 
+ *
+ * https://www.youtube.com/watch?v=lMrz7ISoDGs&index=54&list=PLIljB45xT85AYIeGfDQwHM8i6PQEDnnTI
+ *
+ */
 
 var productsFromAry = require('./src/productsFromAry')
 var modPow = require('./src/powMod')
