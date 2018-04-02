23fd1486b18cc42972e85161b99077066d528735
Dennis Iversen <dennis.iversen@gmail.com>
2018-04-02 23:36:38 +0200

    auto commit

--- src/sieveTotients.js
+++ src/sieveTotients.js
@@ -1,10 +1,21 @@
+/** 
+ * Readable version of totient sieve
+*/
+
 var primeSieve = require('./sieve-of-eratosthenes')
 
 function sieve (totients, num) {
-    let iterations = Math.ceil(totients.length / num)
+    
 
+    // If prime
     totients[num]--
+
+    // Calculate number of iterations of num
+    let iterations = Math.ceil(totients.length / num)
     for(let i = 2; i < iterations; i++ ) {
+
+        // Use eulers totient function
+        // φ(n) = n *(1 - 1/p1)* ... (1 - 1/pn).
         totients[i*num] = totients[i*num] * (1 - (1/num))
     }
     return totients
@@ -12,20 +23,25 @@ function sieve (totients, num) {
 
 function sieveTotients (length) {
     
+    // Primes
     var primes = primeSieve(length)
 
+    // Initialize
     var totients = []
     for(let i = 0; i <= length; i++) {
         totients.push(i) 
     }
-    
+
+    // Sieve
     for(let i = 0; i < primes.length; i++) {
         totients = sieve(totients, primes[i]) 
     }
     
+    // Parse integers
     totients.forEach( (v, i) => {
         totients[i] = parseInt(v)
     })
+
     return totients
 }
 
