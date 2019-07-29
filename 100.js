23c3b8860a2cec49e395ce1e8514b571421660bf
Dennis Iversen <dennis.iversen@gmail.com>
2019-07-29 21:46:37 +0200

    auto commit

---
+++ 100.js
@@ -0,0 +1,24 @@
+// Done using Wolfram alpha
+// 
+// (x/n) * ( (x-1)/ (n-1)) = 1/2 , n > 1
+//
+// https://www.wolframalpha.com/input/?i=(x%2Fn)+*+(+(x-1)%2F+(n-1))+%3D+1%2F2+,+n+%3E+1
+//
+// First solution beyond 10^12
+// 
+// n = 1/4 (-(3 - 2 sqrt(2))^m - sqrt(2) (3 - 2 sqrt(2))^m - (3 + 2 sqrt(2))^m +
+// sqrt(2) (3 + 2 sqrt(2))^m + 2) where m = 17 
+// => 1.07038x10^12
+//
+// https://www.wolframalpha.com/input/?i=n+%3D+1%2F4+(-(3+-+2+sqrt(2))%5Em+-+sqrt(2)+(3+-+2+sqrt(2))%5Em+-+(3+%2B+2+sqrt(2))%5Em+%2B+sqrt(2)+(3+%2B+2+sqrt(2))%5Em+%2B+2)+where+m+%3D+17
+//
+// x = 1/8 (2 (3 - 2 sqrt(2))^m + sqrt(2) (3 - 2 sqrt(2))^m + 2 (3 + 2
+// sqrt(2))^m - sqrt(2) (3 + 2 sqrt(2))^m + 4) where m = 17
+// => 756872327473
+// 
+// 
+// https://www.wolframalpha.com/input/?i=x+%3D+1%2F8+(2+(3+-+2+sqrt(2))%5Em+%2B+sqrt(2)+(3+-+2+sqrt(2))%5Em+%2B+2+(3+%2B+2+sqrt(2))%5Em+-+sqrt(2)+(3+%2B+2+sqrt(2))%5Em+%2B+4)+where+m+%3D+17
+//
+// 
+//
+// Solution is 756872327473
